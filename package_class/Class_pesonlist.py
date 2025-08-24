550d894bbe0415eb711b2441cfac2c29c2987eda
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-08-24 18:31:13 +0300

    initialization class PersonCard, Node, PesonList

---
+++ package_class/Class_pesonlist.py
@@ -0,0 +1,36 @@
+class PersonCard:
+    def __init__(self, name: str, age: int, occupation: str):
+        self.__name = self.__check_name(name)
+        self.__age = self.__check_age(age)
+        self.__occupation = self.__check_occupation(occupation)
+
+    def __check_name(self, name):
+        if not isinstance(name, str):
+            name = str(name)
+            # Выполняется детальная проверка на соответствие name имени персоны в цикле while
+            return name
+
+    def __check_age(self, age):
+        while not isinstance(age, int) and (14 <= age or age <= 90):
+            print('Возраст персоны должен быть целым числом в интервале 14 - 90 лет')
+            age = input('Введите возраст в интервале 14 - 90 лет')
+            # Выполняется детальная проверка на соответствие age возрасту персоны в цикле while
+            return age
+
+    def __check_occupation(self, occupation):
+        if not isinstance(occupation, str):
+            occupation = str(occupation)
+            # Выполняется детальная проверка на соответствие name имени персоны в цикле while
+            return occupation
+
+
+class PesonList:
+    def __init__(self):
+        self.__head = None
+        self.__count = 0
+
+    class Node:
+        def __init__(self, data: PersonCard, next=None):
+            self.__data = data
+            self.__next = next
+
