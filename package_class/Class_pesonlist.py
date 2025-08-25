77fdfb70c37f94f1d6b89b7b6c93dea28927c99a
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-08-25 21:02:17 +0300

    add function insert_person_at

--- package_class/Class_pesonlist.py
+++ package_class/Class_pesonlist.py
@@ -15,12 +15,12 @@ class PersonCard:
             print('Возраст персоны должен быть целым числом в интервале 14 - 90 лет')
             age = input('Введите возраст в интервале 14 - 90 лет')
             # Выполняется детальная проверка на соответствие age возрасту персоны в цикле while
-            return age
+            return int(age)
 
     def __check_occupation(self, occupation):
         if not isinstance(occupation, str):
             occupation = str(occupation)
-            # Выполняется детальная проверка на соответствие name имени персоны в цикле while
+            # Выполняется детальная проверка на соответствие occupation профессия персоны в цикле while
             return occupation
 
 
@@ -38,11 +38,19 @@ class PesonList:
     def is_empty(self):
         return self.__count == 0
 
-    def add_person(self, name, age, occupation):
+    def add_person(self, name: str, age: int, occupation: str) -> None:
+        """
+        Функция добавляет карточку class PersonCard в начало списка
+        :param name: поле class PersonCard
+        :param age: поле class PersonCard
+        :param occupation: поле class PersonCard
+        :return:
+        """
+
         person = PersonCard(name, age, occupation)
         node = PesonList.Node(person)
 
-        if self.__count == 0:
+        if self.is_empty():
             self.__tail = node
 
         else:
@@ -51,11 +59,18 @@ class PesonList:
         self.__head = node
         self.__count += 1
 
-    def append_person(self, name, age, occupation):
+    def append_person(self, name: str, age: int, occupation: str) -> None:
+        """
+        Функция добавляет карточку class PersonCard в конец списка
+        :param name: поле class PersonCard
+        :param age: поле class PersonCard
+        :param occupation: поле class PersonCard
+        :return:
+        """
         person = PersonCard(name, age, occupation)
         node = PesonList.Node(person)
 
-        if self.__count == 0:
+        if self.is_empty():
             self.__head = node
 
         else:
@@ -64,21 +79,36 @@ class PesonList:
         self.__tail = node
         self.__count += 1
 
-    def insert_person_at(self, index, name, age, occupation):
+    def insert_person_at(self, index: int, name: str, age: int, occupation: str) -> None:
+        """
+        Функция вставляет элемент по номеру в списке
+        Пример: index = 3, data = A
+        номера позиций в списке 1,2,3,4,5
+        ответ:
+        1,2,А,3,4,5
+        :param index: номер позиции вставки
+        :param name: поле класса PersonCard
+        :param age: поле класса PersonCard
+        :param occupation: поле класса PersonCard
+        :return: None
+        """
         assert isinstance(index, int), f'Ожидалось: <class int>, получили: {type(index)}'
 
-        if index > self.__count or index < 0:
-            raise ValueError(f'index позиции должен быть >= 0 или <= {self.__count}')
+        if index > self.__count or index < 1:
+            raise ValueError(f'index позиции должен быть >= 1 или <= {self.__count}')
 
         person = PersonCard(name, age, occupation)
         node = PesonList.Node(person)
-
         real = self.__head
 
-        if index <= middle:
-            for i in range(1, index):
+        if index == 1:
+            node.next = self.__head
+            self.__head = node
+
+        else:
+            for _ in range(1, index - 1):
                 real = real.next
-            node.next = real
-            real = node
 
-
+        node.next = real.next
+        real.next = node
+        self.__count += 1
